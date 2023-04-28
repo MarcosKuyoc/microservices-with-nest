af18092736c2afb129e26ab9b46cf797a2e5971c
Marcos Kuyoc <marcos.kuyoc.dev@gmail.com>
2023-04-27 19:04:55 -0600

    chore: se renombra la carpteta de archivos compartidos

---
+++ apps/shared/authorization/authorization.guard.ts
@@ -0,0 +1,40 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { expressJwtSecret } from 'jwks-rsa';
+import { promisify } from 'util';
+import * as jwt from 'express-jwt';
+import { AUTH0_AUDIENCE, AUTH0_CLIENT_URL } from '../keys/auth0';
+
+@Injectable()
+export class AuthorizationGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.getArgByIndex(0);
+    const res = context.getArgByIndex(1);
+
+    const checkJwt = promisify(
+      jwt({
+        secret: expressJwtSecret({
+          cache: true,
+          rateLimit: true,
+          jwksRequestsPerMinute: 5,
+          jwksUri: `${AUTH0_CLIENT_URL}.well-known/jwks.json`,
+        }),
+        audience: AUTH0_AUDIENCE,
+        issuer: AUTH0_CLIENT_URL,
+        algorithms: ['RS256'],
+      }),
+    );
+
+    try {
+      await checkJwt(req, res);
+      return true;
+    } catch (error) {
+      console.log(error);
+      throw new UnauthorizedException(error);
+    }
+  }
+}
